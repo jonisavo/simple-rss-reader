371a9d31fbfe3ca529878ef3b87e85498a50c73a
Joni Savolainen <joni@savolainen.io>
2020-11-01 13:28:37 +0200

    Use AsyncStorage to store RSS urls

--- src/components/ChannelContext.jsx
+++ src/components/ChannelContext.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-
-import { channels } from '../channels'
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const ChannelContext = React.createContext();
 
 const ChannelProvider = (props) => {
-    // THIS CODE WAS USED TO FETCH RSS URLs FROM MY WEB SERVER.
-    // IT CAUSED AWFUL LOAD TIMES, THOUGH, SO THE RSS URLs ARE HARDCODED
-    // IN /src/channels.jsx FOR NOW.
-    /*
     const [channels, setChannels] = React.useState([]);
     const [isFetchingChannels, setIsFetchingChannels] = React.useState(true);
+    const { getItem, setItem } = useAsyncStorage('@urls');
 
-    const updateChannels = (urls) => setChannels(urls.filter(url => url.length > 0));
+    const readUrlsFromStorage = async () => {
+        const urls = JSON.parse(await getItem());
+        setChannels(urls || []);
+        setIsFetchingChannels(false);
+    }
+
+    const saveUrl = async (url) => {
+        const urls = JSON.parse(await getItem()) || [];
+        urls.push(url);
+        setItem(JSON.stringify(urls));
+        setChannels(urls);
+    }
 
     React.useEffect(() => {
-        // Fetch all RSS URLs
-        fetch('https://static.jonisavo.xyz/rss.txt')
-            .then(response => response.text())
-            .then(str => str ? str.split('\n') : [])
-            .then(lines => updateChannels(lines))
-            .catch(error => alert(error))
-            .finally(() => setIsFetchingChannels(false));
-    }, [])
-    */
-
-    const isFetchingChannels = false;
+        readUrlsFromStorage();
+    }, []);
    
     return (
-        <ChannelContext.Provider value={{ channels, isFetchingChannels }}>
+        <ChannelContext.Provider value={{ channels, isFetchingChannels, saveUrl }}>
             {props.children}
         </ChannelContext.Provider>
     )
