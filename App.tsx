fcc571a4d99f4db844af89cbc970355df342fd7e
Joni Savolainen <joni@savolainen.io>
2021-02-09 12:39:28 +0200

    Convert App.jsx to TypeScript

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+import Navigator from './src/components/Navigator';
+import { ChannelProvider } from './src/components/ChannelContext';
+
+export default function App(): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    CourierPrime: require('./assets/fonts/CourierPrime-Regular.ttf'),
+    'CourierPrime-Bold': require('./assets/fonts/CourierPrime-Bold.ttf'),
+    'CourierPrime-Italic': require('./assets/fonts/CourierPrime-Italic.ttf'),
+    SourceSansPro: require('./assets/fonts/SourceSansPro-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ChannelProvider>
+      <StatusBar style="auto" />
+      <Navigator />
+    </ChannelProvider>
+  );
+}
